63f2e67dab039b7b5d7057b95ad77748e8af1a91
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2025-01-25 02:03:10 +0100

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx,html}', // Include all file paths where Tailwind is used
   ],
   prefix: "",
   theme: {
